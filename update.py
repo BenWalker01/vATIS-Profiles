a169b848726abfc99594f7ff75d95904c5b6717b
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-01-02 20:06:05 +0000

    further updaets

--- update.py
+++ update.py
@@ -2,25 +2,30 @@ import os
 import json
 from datetime import datetime
 
-profiles_dir = 'profiles'
-update_serial = datetime.now().strftime("%Y%m%d01")
+input_dir = 'data'
+output_dir = 'profiles'
+update_serial = 1970010100
 
-for filename in os.listdir(profiles_dir):
+for filename in os.listdir(input_dir):
     if filename.endswith('.json'):
         name = os.path.splitext(filename)[0]
-        filepath = os.path.join(profiles_dir, filename)
-        with open(filepath, 'r') as file:
+        input_filepath = os.path.join(input_dir, filename)
+        output_filepath = os.path.join(output_dir, filename)
+        
+        with open(input_filepath, 'r') as file:
             data = json.load(file)
         
-        update_url = f"https://raw.githubusercontent.com/BenWalker01/vATIS-Profiles/refs/heads/main/{name}.json"
+        update_url = f"https://raw.githubusercontent.com/BenWalker01/vATIS-Profiles/refs/heads/main/data/{name}.json"
         
-        # Add updateUrl and updateSerial under the Name field
-        if "Name" in data:
-            data["updateUrl"] = update_url
-            data["updateSerial"] = str(update_serial)  # Ensure updateSerial is a string
+        new_data = {
+            "Name": data.get("Name",name),
+            "updateUrl": update_url,
+            "updateSerial": update_serial,
+            "stations": data.get("Composites", [])
+        }
         
-        # Write the updated data back to the file
-        with open(filepath, 'w') as file:
-            json.dump(data, file, indent=2)
+        # Write the updated data to the output file
+        with open(input_filepath, 'w') as file:
+            json.dump(new_data, file, indent=2)
         
         print(f"Updated {filename}")
